91c09b44370e541232d3c2d9e36d762086f71aa3
rwcorbett <robb.corbett@gmail.com>
2015-12-06 16:12:50 -0500

    cleanup JS
    - update file header
    - update the HTML that is generated

--- collapse_elem.js
+++ collapse_elem.js
@@ -1,34 +1,38 @@
-//
-// NAME   : collapse_elem.js
-// AUTHOR : Robb Corbett
-// EMAIL  : robb.corbett@live.ca || rwcorbett@live.ca
-// DESC   : this library provides a simple way to add a collapsible element anywhere in your page
-//
-// Copyright (c) 2015 Robb Corbett
-//
+/**
+ * NAME   : collapse_elem.js
+ * AUTHOR : Robb Corbett @rwcorbett
+ * EMAIL  : robb.corbett@live.ca || rwcorbett@live.ca
+ * GITHUB : https://github.com/rwcorbett/collapse_elem.git
+ * DESC   : this library provides a simple way to add a collapsible element anywhere in your page
+ *
+ * Copyright (c) 2015 Robb Corbett
+ *
+ */
 
-//my own expander library
 var Collapse_Elem = Collapse_Elem | {};
 Collapse_Elem = function () {
     Collapse_Elem.prototype.load = function (fromTop, fromLeft) {
-        //get expander items
+        //** get expander items
         var expanderBlocks = document.getElementsByClassName("collapse_elem");
-        //build each and collapse
+        var expanderInner = "<span style='font-size:75%;font-family:\"Source Code Pro\", Consolas, \"Courier New\", monospace;'>&nbsp;click to expand.</span>";
+        //** build each and collapse
         for (var i = 0; i < expanderBlocks.length; i++) {
             var expander = document.createElement("i");
             expander.className = " expander fa fa-plus-square";
-            expander.innerHTML = "<span>&nbsp;&larr; click to expand.</span>";
+            expander.innerHTML = expanderInner;
             expander.style.cursor = "pointer";
             expander.style.display = "inline";
-            expander.style.padding = "0 0.3em 0 0";
+            expander.style.padding = "0 0.25em 0 0";
             expander.style.position = "absolute";
-            expander.style.top = fromTop && fromTop != "" && typeof fromTop == "string" ? fromTop : "0.1em";
-            expander.style.left = fromLeft && fromLeft != "" && typeof fromLeft == "string" ? fromLeft : "-1.1em";
+            expander.style.top = fromTop && fromTop != "" && typeof fromTop == "string" ? fromTop : "-0.5em";
+            expander.style.left = fromLeft && fromLeft != "" && typeof fromLeft == "string" ? fromLeft : "0em";
             var element = expanderBlocks[i];
             var elementInner = element.innerHTML;
             var hiddenInner = document.createElement("span");
             hiddenInner.innerHTML = elementInner;
             hiddenInner.style.display = "none";
+            hiddenInner.style.position = "relative";
+            hiddenInner.style.top = "0.25em";
             hiddenInner.className = " collapse collapsed";
             element.innerHTML = "";
             element.style.position = "relative";
@@ -36,9 +40,9 @@ Collapse_Elem = function () {
             element.insertAdjacentHTML("afterbegin", expander.outerHTML);
             element.appendChild(hiddenInner);
         }
-        //get buttons
+        //** get buttons
         var expanderButtons = document.getElementsByClassName("expander");
-        //add click event to each one
+        //** add click event to each one
         for (var i = 0; i < expanderButtons.length; i++) {
             expanderButtons[i].addEventListener('click', function (e) {
                 var E = this.parentElement.getElementsByClassName('collapse')[0];
@@ -51,7 +55,7 @@ Collapse_Elem = function () {
                     E.className = " collapse collapsed";
                     E.style.display = "none";
                     this.className = " expander fa fa-plus-square";
-                    this.innerHTML = "<span>&nbsp;&larr; click to expand.</span>";
+                    this.innerHTML = expanderInner;
                 }
                 e.preventDefault();
             }, true);
